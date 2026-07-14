8f6cdbb13b0009e86e327c7e5de481263cf40491
Matheus <matheussouza102030@gmail.com>
2026-07-14 20:37:43 -0300

    Atividade3- finalizado

--- src/atividades/atividade3/CalculadoraMetodosEncadeados.java
+++ src/atividades/atividade3/CalculadoraMetodosEncadeados.java
@@ -1,6 +1,32 @@
 package atividades.atividade3;
 
+import java.util.Scanner;
+
 public class CalculadoraMetodosEncadeados {
 
 
+    public static int subtrairNumeros(int x, int y) {
+        int resultado = x - y;
+        return resultado;
+    }
+
+    public static void imprimirResultado(int valor) {
+        System.out.println("O resultado da operação é: " + valor);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número (x): ");
+        int numero1 = entrada.nextInt();
+
+        System.out.print("Digite o segundo número (y): ");
+        int numero2 = entrada.nextInt();
+
+
+        int valorCalculado = subtrairNumeros(numero1, numero2);
+
+        imprimirResultado(valorCalculado);
+    }
 }
--- src/atividades/atividade3/VerificadorDeAcessoSeguranca.java
+++ src/atividades/atividade3/VerificadorDeAcessoSeguranca.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
+package atividades.atividade3;
 
+import java.util.Scanner;
 public class VerificadorDeAcessoSeguranca {
 
 
